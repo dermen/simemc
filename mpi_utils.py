c5afe7998fa71e6ecffedf2f61b03dfaff99ebc5
dermen <dermen@lbl.gov>
2022-02-12 17:56:01 -0800

    adds safe mpi makedir

--- mpi_utils.py
+++ mpi_utils.py
@@ -1,5 +1,6 @@
 from mpi4py import MPI
 COMM = MPI.COMM_WORLD
+import os
 
 
 def print0(*args, **kwargs):
@@ -9,3 +10,10 @@ def print0(*args, **kwargs):
 
 def printR(*args, **kwargs):
     print("rank%d"%COMM.rank,*args, **kwargs)
+
+
+def make_dir(dirname):
+    if COMM.rank==0:
+        if not os.path.exists(dirname):
+            os.makedirs(dirname)
+    COMM.barrier()
