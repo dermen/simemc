fixes gradient scaling error

emc_updaters.py
@@ -144,7 +144,7 @@ class DensityUpdater(Updater):
         self.LOGGER.debug("Done with rank grad/functional (iter %d)" % (self.iter_num+1))
         COMM.barrier()
         self.LOGGER.debug("Reducing grad")
-        emc.L.reduce_density_derivs(COMM)
+        emc.L.allreduce_density_derivs(COMM)
         COMM.barrier()
         self.LOGGER.debug("functional")
         functional = COMM.reduce(functional)
@@ -152,14 +152,10 @@ class DensityUpdater(Updater):
 
         # Because we reparameterized, we must scale the grad -> dW/dx *grad
         self.LOGGER.debug("Scaling the gradient (iter %d)" % (self.iter_num+1))
-        minus_grad = None
-        if COMM.rank == 0:
-            minus_grad = emc.L.reparameterized_densities_gradient()
+        minus_grad = emc.L.reparameterized_densities_gradient(x)
         self.LOGGER.debug("Bcasting the Scaled gradient (iter %d)" % (self.iter_num+1))
-        # TODO time different BCast methods,also time REduceAll above on grad to potentially  avoid this bcast
-        minus_grad = mpi_utils.bcast_large(minus_grad, verbose=True, comm=COMM)
-        emc_s = "Done with emc iter num: %d (F=%f,G=%10.7g, |x|=%f)" \
-                % (self.iter_num+1, functional, np.mean(minus_grad),  np.linalg.norm(x))
+        emc_s = "Done with emc iter num: %d (F=%f)" \
+                % (self.iter_num+1, functional)
         self.LOGGER.debug(emc_s)
 
         # running a minimizer, so return the negative loglike and its gradient

tests/test_reparam_kernels.py
@@ -60,8 +60,9 @@ def main():
         L.dens_deriv(finite_rot_inds, finite_P_dr, verbose=False, shot_scale_factor=1,
                      reset_derivs=False, return_grad=False)
     dens_deriv = L.densities_gradient()
-    dens_deriv_reparam = - dens_deriv * (dens_deriv / np.sqrt(dens_deriv**2 + 1))
-    dens_deriv_reparam2 = L.reparameterized_densities_gradient()
+    scale_factor = reparam_dens / np.sqrt(reparam_dens**2 + 1)
+    dens_deriv_reparam = - dens_deriv * scale_factor
+    dens_deriv_reparam2 = L.reparameterized_densities_gradient(reparam_dens)
 
     assert np.allclose(dens_deriv_reparam, dens_deriv_reparam2)
     print("OK")